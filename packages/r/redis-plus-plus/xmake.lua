41a6df8c19fa29e8c7e120f14517ef7a60ed2788
fantasy-peak <1356346239@qq.com>
2022-07-13 20:18:22 +0800

    add reddis-plus-plus

---
+++ packages/r/redis-plus-plus/xmake.lua
@@ -0,0 +1,22 @@
+package("redis-plus-plus")
+set_homepage("https://github.com/sewenew/redis-plus-plus.git")
+set_description("")
+set_license("Apache-2.0 License")
+
+add_urls("https://github.com/sewenew/redis-plus-plus.git")
+
+add_deps("cmake")
+add_deps("hiredis", {system = false})
+add_deps("boost", {system=false, configs = {all = true}})
+add_deps("libuv", {system = false})
+
+on_install("linux", function (package)
+    local configs = {}
+    table.insert(configs, "-DREDIS_PLUS_PLUS_BUILD_ASYNC=libuv")
+    table.insert(configs, "-DREDIS_PLUS_PLUS_ASYNC_FUTURE=boost")
+    table.insert(configs, "-DREDIS_PLUS_PLUS_CXX_STANDARD=20")
+    table.insert(configs, "-DREDIS_PLUS_PLUS_BUILD_TEST=OFF")
+    table.insert(configs, "-DREDIS_PLUS_PLUS_BUILD_SHARED=OFF")
+    import("package.tools.cmake").install(package, configs)
+end)
+package_end()
